47433b1a8b674c536f3cc423d95d8f5cc249db15
Zubair Baig <139370924+Zeilous12@users.noreply.github.com>
2025-12-24 05:23:43 +0000

    Fix headers issue and added images

--- app/lib/skuParser.ts
+++ app/lib/skuParser.ts
@@ -175,7 +175,17 @@ export async function fetchFirstProductDetailsHTML(
     /* -------------------------
        4. Fetch product page
     -------------------------- */
-    const productRes = await fetch(productUrl);
+    const productRes = await fetch(productUrl, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+        "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      "Accept":
+        "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Referer": "https://www.google.com/",
+    }
+  });
     if (!productRes.ok) return 'Failed to fetch product page';
 
     const productHtml = await productRes.text();
